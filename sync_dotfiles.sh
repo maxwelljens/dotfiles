8c5e4ac43406f4e84b9f388f6639efd7964d4f26
maxwelljens <85795372+maxwelljens@users.noreply.github.com>
2022-02-06 13:28:15 +0100

    Update

---
+++ sync_dotfiles.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# bspwm
+mkdir bspwm &> /dev/null; cp ~/.config/bspwm/bspwmrc ./bspwm/
+# sxhkd
+mkdir sxhkd &> /dev/null; cp ~/.config/sxhkd/sxhkdrc ./sxhkd/
+# kitty
+mkdir kitty &> /dev/null; cp ~/.config/kitty/kitty.conf ./kitty/
+# dunst
+mkdir dunst &> /dev/null; cp ~/.config/dunst/dunstrc ./dunst/
+# rofi
+mkdir rofi &> /dev/null; cp ~/.config/rofi/config.rasi ./rofi/
+# polybar
+mkdir polybar &> /dev/null; cp ~/.config/polybar/config ./polybar/
+mkdir polybar &> /dev/null; cp ~/.config/polybar/launch.sh ./polybar/
+# vim
+mkdir vim &> /dev/null; cp ~/.config/nvim/init.vim ./nvim/.vimrc
+# zsh
+cp ~/.zshrc .
+# background image
+cp ~/.bg.png .
